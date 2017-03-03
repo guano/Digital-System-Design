7dff6f14e8cc4684d5c36f4637ba2ff7f059d46c
Taylor <taycowley@gmail.com>
2017-03-03 11:54:29 -0700

    Lab 1: Xilinx ISE Design Suite Description

---
+++ Lab1_EE320_Taylor_Cowley/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "J:/EE 320/Lab1_EE320_Taylor_Cowley/even_detector_testbench_isim_beh.exe" -prj "J:/EE 320/Lab1_EE320_Taylor_Cowley/even_detector_testbench_beh.prj" "work.even_detector_testbench" 
---
+++ Lab1_EE320_Taylor_Cowley/listing_2_1.tcl
@@ -0,0 +1,24 @@
+put a 000
+run 100 ns
+put a 111
+run 100 ns
+put a 000
+run 100ns
+put a 110
+run 100ns
+put a 000
+run 100ns
+put a 101
+run 100ns
+put a 100
+run 100 ns
+put a 011
+run 100ns
+put a 000
+run 100ns
+put a 010
+run 100ns
+put a 001
+run 100ns
+put a 000 
+run 100ns
